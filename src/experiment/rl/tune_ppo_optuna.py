32f3dfadf378b28d69a8eed43b980b7218f97083
anasbayu <anasbayu03@gmail.com>
2025-11-24 10:20:16 +0000

    chore: change optuna param tuning

--- src/experiment/rl/tune_ppo_optuna.py
+++ src/experiment/rl/tune_ppo_optuna.py
@@ -20,10 +20,10 @@ torch.serialization.add_safe_globals(safe_globals)
 N_TRIALS = 35          # How many different hyperparams to try
 N_STARTUP_TRIALS = 5   # Don't prune the first 5 trials
 N_EVALUATIONS = 5      # How many times to check performance per trial
-TIMESTEPS_PER_TRIAL = 2_000_000 # Short runs for speed (2M steps)
+TIMESTEPS_PER_TRIAL = 6_000_000 # Short runs for speed (6M steps)
 NUM_ENV = 5            # Keep your 5 environments
-STUDY_NAME = "ppo_metadrive_study" # Name for the database entry
-STORAGE_URL = "sqlite:///optuna_study.db" # The file where results are saved
+STUDY_NAME = "ppo_metadrive_study_6M" # Name for the database entry
+STORAGE_URL = "sqlite:///optuna_study_6M.db" # The file where results are saved
 # =========================
 
 # Define the training config (Same as your main script)
